4413e0906e72d027136ffc56a677ed2cbf162864
Adam Cmiel <adam@famo.us>
2015-05-08 10:46:35 -0700

    clean up source

--- Nerdfeed/ZENCoursesViewController.swift
+++ Nerdfeed/ZENCoursesViewController.swift
@@ -45,7 +45,6 @@ class ZENCoursesViewController: UITableViewController {
             let deserializedData: AnyObject? = NSJSONSerialization.JSONObjectWithData(data!, options: NSJSONReadingOptions.MutableContainers, error: nil)
             
             self.courses = Courses(data: deserializedData!["courses"] as! Array<AnyObject>)
-            println(self.courses!)
             
             dispatch_async(dispatch_get_main_queue()) {
                 println("reloading the table \(self.courses!.count)")
@@ -69,29 +68,16 @@ class ZENCoursesViewController: UITableViewController {
     // MARK: - Table view data source
 
     override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
-        // #warning Potentially incomplete method implementation.
-        // Return the number of sections.
-        
-        println("calling \(__FUNCTION__)")
         return 1
     }
 
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         let numberOfRows = self.courses?.count
-        
-        println("rendering \(numberOfRows) courese")
-        
-        if (numberOfRows == nil) {
-            return 0
-        }
-        else {
-            return numberOfRows!
-        }
+        return (numberOfRows == nil) ? 0 : numberOfRows!
     }
 
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         
-        println("generating cell for row \(indexPath.row)")
         let cell = tableView.dequeueReusableCellWithIdentifier(cellReuseIdentifier, forIndexPath: indexPath) as! UITableViewCell
         
         var course = self.courses![indexPath.row]
@@ -108,50 +94,4 @@ class ZENCoursesViewController: UITableViewController {
         
         self.navigationController?.pushViewController(self.webViewController!, animated: true)
     }
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        // Return NO if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
-        if editingStyle == .Delete {
-            // Delete the row from the data source
-            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
-        } else if editingStyle == .Insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        // Return NO if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using [segue destinationViewController].
-        // Pass the selected object to the new view controller.
-    }
-    */
-    
 }
